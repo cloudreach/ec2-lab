c6b17b83cce91e55b44cb3db7ca60cea1e9fb426
Nisha Bidave <nisha.bidave@cloudreach.com>
2022-07-21 12:56:10 +0530

    pushing data

---
+++ data.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+    owners = ["099720109477"] # Canonical
+}
+
+data "aws_vpc" "talent-academy" {
+    filter {
+    name   = "tag:Name"
+    values = ["lab_vpc"]
+  }
+
+}
+
+data "aws_subnet" "public_subnet" {
+    filter {
+    name   = "tag:Name"
+    values = ["public"]
+  }
+
+}
